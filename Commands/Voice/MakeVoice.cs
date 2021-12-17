1d4e15a183fe62770fa2fb529d7cb9e096e83cf1
Oliver <oli70963@gmail.com>
2021-12-17 16:41:29 +0100

    parancsleírások frissítése

--- Commands/Voice/MakeVoice.cs
+++ Commands/Voice/MakeVoice.cs
@@ -49,24 +49,24 @@ public static class MakeVoice
                     .WithDescription("A sorban lévő zenék törlése"),
                 new SlashCommandBuilder()
                     .WithName("bassboost")
-                    .WithDescription("Basszus erősítés bekapcsolása / kikapcsolása"),
+                    .WithDescription("Basszus erősítés bekapcsolása"),
                 new SlashCommandBuilder()
                     .WithName("nightcore")
-                    .WithDescription("Nightcore mód bekapcsolása / kikapcsolása"),
+                    .WithDescription("Nightcore mód bekapcsolása"),
                 new SlashCommandBuilder()
                     .WithName("8d")
-                    .WithDescription("8D mód bekapcsolása / kikapcsolása"),
+                    .WithDescription("8D mód bekapcsolása"),
                 new SlashCommandBuilder()
                     .WithName("vaporwave")
-                    .WithDescription("Vaporwave mód bekapcsolása / kikapcsolása"),
+                    .WithDescription("Vaporwave mód bekapcsolása"),
                 new SlashCommandBuilder()
                     .WithName("speed")
-                    .WithDescription("Zene sebességének beállítása")
+                    .WithDescription("Zene sebességének növelése")
                     .AddOption("speed", ApplicationCommandOptionType.Integer, "Sebesség számban megadva (1-10)", true,
                         minValue: 1, maxValue: 10),
                 new SlashCommandBuilder()
                     .WithName("pitch")
-                    .WithDescription("Zene hangmagasságának beállítása")
+                    .WithDescription("Zene hangmagasságának növelése")
                     .AddOption("pitch", ApplicationCommandOptionType.Integer, "Hangmagasság számban megadva (1-10)",
                         true, minValue: 1, maxValue: 10),
                 new SlashCommandBuilder()
